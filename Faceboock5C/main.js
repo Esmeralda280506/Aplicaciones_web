c1aab973a03a517eda0a079734d0b13e62d4bb94
Esmeralda280506 <baronesme2302@gmail.com>
2026-02-10 23:06:26 -0600

    actualizacion de pagina

--- Faceboock5C/main.js
+++ Faceboock5C/main.js
@@ -1,42 +1,39 @@
-const selectUsuario = document.getElementById("select-usuerio")
-const muroDiv = document.getElementById("muro")
-const avatarImg = document.getElementById("avatar-img")
-const nombreHeader = document.getElementById("nombre-usuario")
+const selectUsuario = document.getElementById('select-usuario');
+const muroDiv = document.getElementById('muro');
+const avatarImg = document.getElementById('avatar-id');
+const nombreHeader = document.getElementById('nombre-usuario');
 
 //Cargamos los usuarios en el select
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(usuarios => {
-        usuarios.forEach(usuario => {
-            const opcion = '<option value="' + usuario.id + '">' + usuario.name + '</option>'
-            selectUsuario.innerHTML += opcion
-        })
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(usuarios => {
+    usuarios.forEach(usuario => {
+        const option = `<option value='${usuario.id}'>${usuario.name}</option>`;
+        selectUsuario.innerHTML += option;
     })
+  })
 
-
-//que va a pasar cada que selecciono un usuario
 const cargaMuro = () => {
-    const userId = selectUsuario.value
-    const nombre = selectUsuario.opcions[selectUsuario.selectIndex].text
+    const userId = selectUsuario.value;
+    const nombre = selectUsuario.options[selectUsuario.selectedIndex].text;
 
-    //mostramos el nombre de usuario y su avatar
-    nombreHeader.innerText = nombre
-    avatarImg.src = "https://api.dicebear.com/9.x/dylan/svg?seed=" + nombre
-    avatarImg.style.display = "block"
+    nombreHeader.innerText = nombre;
+    avatarImg.src = "https://api.dicebear.com/9.x/bottts/svg?seed=" + nombre;
+    avatarImg.style.display = 'block';
 
-    //cargamos el muro
-    fetch("https://jsonplaceholder.typicode.com/users/" + userId + "/posts")
-        .then(response => response.json())
-        .then(posts => {
-            //limpiaos el muro por si hay post de otro usuario
-            muroDiv.innerHTML = ""
-            //recorremos post por post para dibujarlos
-            posts.forEach(post => {
-                muroDiv.innerHTML += '<div class="post">' +
-                    '<div class="post-title">' + post.title + '</div>'
-                    + '<p>' + post.body + '</p>'
-                    + '<small>Publicado por : ' + nombre + '</small>'
-                    + '</div>'
+    //Cargar posts del usuario
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
+        .then (response => response.json())
+        .then (posts => {
+            muroDiv.innerHTML = ""; //Limpiamos el muro
+            posts.forEach(post => { 
+                muroDiv.innerHTML += `
+                    <div class="post">
+                        <div class="post-title">${post.title}</div>
+                        <p class="post-body">${post.body}</p>
+                        <small>Publicado por: ${nombre}</small>
+                    </div>
+                `;
             })
         })
 }
