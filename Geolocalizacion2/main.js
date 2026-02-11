50393b504e98a4499dcff1361f9d75bddecce242
Esmeralda280506 <baronesme2302@gmail.com>
2026-02-10 23:26:52 -0600

    actualizacion de pagina

--- Geolocalizacion2/main.js
+++ Geolocalizacion2/main.js
@@ -1,32 +1,36 @@
 let lat
 let lon
 
-if(navigator.geolocation){
+if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-        (respuesta)=>{
-            lat=respuesta.coords.latitude
-            lon=respuesta.coords.longitude
+        (respuesta) => {
+            lat = 21.059628324181247
+            lon = -98.49375462698794
 
-            const coordenadas=[lat,lon]
+            const coordenadas = [lat, lon]
 
-            let map = L.map('map').setView(coordenadas, 13);
-
+            const map = L.map('map').setView(coordenadas, 13);
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                maxZoom: 22,
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">Mapa 5C</a>'
             }).addTo(map);
 
-            let marcador = L.marker(coordenadas).addTo(map);
-            marcador.bindPopup('<b>Estoy aqui...</b><br>Mis corrdenadas son : <br>latitud : '+lat+'<br>longitud : '+lon).openPopup()
+            let marcador = L.marker(coordenadas).addTo(map)
+            var polygon = L.polygon([
+                [21.059358617128414, -98.49379163967345],
+                [21.05943069407008, -98.49386060005482],
+                [21.059479694373955, -98.49379518341915],
+                [21.059384287779828, -98.49369785004639],
+                [21.05935506556044, -98.49372570989114]
+            ]).addTo(map);
+            marcador.bindPopup('<b>Esoy Aqui ...</b> <br> Mis coordenadas son: <br>Latitud: ' +
+                lat + '<br>Longitud: ' + lon).openPopup();
+            polygon.bindPopup("Hola este es el lugar en donde vivo.");
 
-            //alert(lat+" , "+lon)
         },
-        ()=>{}
+        () => {
 
-
-    )
-
-
-}else{
+        })
+} else {
 
 }
